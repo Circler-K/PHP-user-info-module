cc1978f18b5a13f12347f597221b9816eba6579c
Circler-K <dkdlelgksthf@gmail.com>
2017-11-30 05:34:38 +0900

    make login module

---
+++ login/DB_connect_for_login.php
@@ -0,0 +1,7 @@
+<?php
+	$DB_host="localhost";
+	$DB_user="hi";
+	$DB_pw="hi";
+	$DB_name = 'hi';
+	$DB_connect=mysqli_connect($DB_host,$DB_user,$DB_pw,$DB_name); //$mysqli = new mysqli($host, $user, $pw, $dbName); = for OOP
+?>
