c415160e238b78a5d316bf604e7e96b1de4ca56d
Circler-K <dkdlelgksthf@gmail.com>
2017-12-02 02:27:01 +0900

    join추가 logout추가

---
+++ join/DB_connect_for_join.php
@@ -0,0 +1,7 @@
+<?php
+	$DB_host="localhost";
+	$DB_user="Circler-K";
+	$DB_pw="Circler-K";
+	$DB_name = 'Circler-K';
+	$DB_connect=mysqli_connect($DB_host,$DB_user,$DB_pw,$DB_name); //$mysqli = new mysqli($host, $user, $pw, $dbName);		
+?>
--- login/DB_connect_for_login.php
+++ login/DB_connect_for_login.php
@@ -1,7 +1,7 @@
 <?php
 	$DB_host="localhost";
-	$DB_user="hi";
-	$DB_pw="hi";
-	$DB_name = 'hi';
+	$DB_user="Circler-K";
+	$DB_pw="Circler-K";
+	$DB_name = 'Circler-K';
 	$DB_connect=mysqli_connect($DB_host,$DB_user,$DB_pw,$DB_name); //$mysqli = new mysqli($host, $user, $pw, $dbName); = for OOP
 ?>
