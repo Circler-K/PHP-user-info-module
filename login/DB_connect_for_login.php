aaa27fe4c60d0e762ffeb61d57470f8d0db300cb
Circler-K <dkdlelgksthf@gmail.com>
2017-12-18 09:00:29 +0900

    DB설정

--- login/DB_connect_for_login.php
+++ login/DB_connect_for_login.php
@@ -1,7 +1,7 @@
 <?php
 	$DB_host="localhost";
-	$DB_user="Circler-K";
-	$DB_pw="Circler-K";
-	$DB_name = 'Circler-K';
+	$DB_user="Circler";
+	$DB_pw="Circler";
+	$DB_name = 'ftp_Project';
 	$DB_connect=mysqli_connect($DB_host,$DB_user,$DB_pw,$DB_name); //$mysqli = new mysqli($host, $user, $pw, $dbName); = for OOP
 ?>
